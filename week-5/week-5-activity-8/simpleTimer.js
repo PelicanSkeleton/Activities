c56eb35bdac31b81eb1a17df2f7e683b7a815aa4
PelicanSkeleton <sgphillips80@gmail.com>
2019-10-19 22:21:01 -0400

    Various in class activities finished. Some functioning, some not.

---
+++ week-5/week-5-activity-8/simpleTimer.js
@@ -0,0 +1,41 @@
+// Step 1:
+// Use the following Audio file below:
+
+var audio = document.createElement("audio");
+audio.setAttribute("src", "./raven.mp3");
+
+//  Step 2: do the following between the --- comments below
+//  after 5 seconds, execute the fiveSeconds function
+//  after 10 seconds, execute the tenSeconds function
+//  after 15 seconds, execute the timeUp function
+
+// ---------------------
+setTimeout(fiveSeconds, 5000);
+setTimeout(tenSeconds, 10000);
+setTimeout(timeUp, 15000);
+// ---------------------
+
+// Step 3:
+// Fill in the blanks to these functions.
+function fiveSeconds() {
+  // in the element with an id of `time-left` add an h2 saying About 10 Seconds Left!
+  $("#time-left").html("<h2>About 10 Seconds Left</h2>");
+  // console log 10 seconds left
+  console.log("10 seconds left.");
+}
+
+function tenSeconds() {
+  // in the element with an id of `time-left` add an h2 saying About 5 Seconds Left!
+  $("#time-left").append("<h2>About 5 Seconds Left</h2>");
+  // console log 5 seconds left
+  console.log("5 seconds left.");
+}
+
+function timeUp() {
+  audio.play();
+  // in the element with an id of `time-left` add an h2 saying Time's Up!
+  $("#time-left").append("<h2>Times Up!</h2>");
+  // console log done
+  console.log("Done.");
+  // The following line will play the audio file above
+}
